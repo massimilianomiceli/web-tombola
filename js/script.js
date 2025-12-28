e4b621129e4c296eb4de10bd335827ae6544bb38
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2025-12-28 19:42:01 +0100

    aggiunto codice js per lestrazione dei numeri con termine del gioco una volta estratti tutti i numeri

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
 const bingoTableElement = document.getElementById("bingo-table");
+const drawButtonElement  = document.getElementById("btn-draw");
+const tombolaSquareElement  = document.getElementById("tombolaSquare");
+const calledNumbers = [];
+
 
 for (let index = 1; index < 91; index++) {
     const newBingoNumber = document.createElement("span");
     newBingoNumber.classList.add("tombolaCirce");
     newBingoNumber.textContent = index;
+    newBingoNumber.id = `bingoNumber${index}`;
     bingoTableElement.append(newBingoNumber);
 }
+
+drawButtonElement.addEventListener("click", function(){
+    if (calledNumbers.length < 90) {
+        let calledNumber;
+        do {
+            calledNumber = Math.floor(Math.random() * 90) + 1;
+        } while (calledNumbers.includes(calledNumber));
+
+        console.log(calledNumber);
+        calledNumbers.push(calledNumber);
+        const calledNumbersElement = document.getElementById(`bingoNumber${calledNumber}`);
+        calledNumbersElement.classList.add("tombolaRedCirce");
+        tombolaSquareElement.textContent = calledNumber;
+    } else alert("Attenzione! Hai già estratto tutti i numeri.");
+
+
+});
+
